1300aab4ff3c28b5ee1517ff0f107b38628b99ac
jono3028 <jonowen@hotmail.com>
2017-09-21 10:08:41 -0700

    V0.1.5 - Added modal for updating actual start/finish times

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,13 @@
 // Write your Javascript code.
+$(document).ready(function(){
+  $('#UpdateTimeModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var lapId = button.data('lapsequence')
+    var position = button.data('lapposition')
+
+    var modal = $(this)
+    modal.find('.modal-title').text('Update actual ' + position + ' time for lap ' + lapId)
+    // modal.find('.modal-body input').val(lapId)
+    modal.find('.modal-content').attr('action', '/UpdateTimes/' + lapId + '/' + position)
+  })  
+})
