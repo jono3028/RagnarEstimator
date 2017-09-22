de2869b158438626a510f4f822e08ec435a182e8
jono3028 <jonowen@hotmail.com>
2017-09-22 13:15:27 -0700

    V0.1.7 - Minor formatting changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,12 +2,11 @@
 $(document).ready(function(){
   $('#UpdateTimeModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
-    var lapId = button.data('lapsequence')
-    var position = button.data('lapposition')
+    var lapId = button.data('lapsequence') // Parse lap Id from link attribute
+    var position = button.data('lapposition') // Parse start or finish from link attribute
 
     var modal = $(this)
-    modal.find('.modal-title').text('Update actual ' + position + ' time for lap ' + lapId)
-    // modal.find('.modal-body input').val(lapId)
-    modal.find('.modal-content').attr('action', '/UpdateTimes/' + lapId + '/' + position)
+    modal.find('.modal-title').text('Update actual ' + position + ' time for lap ' + lapId) // Generate infformative modal header
+    modal.find('.modal-content').attr('action', '/UpdateTimes/' + lapId + '/' + position) // Generate path for form action
   })  
 })
