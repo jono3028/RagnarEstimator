adf0d71e62421fd351845becce26e33b0d4a08fd
jono3028 <jonowen@hotmail.com>
2017-09-26 11:38:44 -0700

    V0.1.10 - Required and validation added to UpdateTimeViewModel

--- Models/UpdateTimeViewModel.cs
+++ Models/UpdateTimeViewModel.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RagnarEstimator.Models
 {
   public class UpdateTimeViewModel
   {
+    [Required(ErrorMessage = "Time is required")]
+    [RegularExpression(@"^[0-9]{1,2}:[0-9]{2}")]
     public string newTime {get; set;}
   }
 }
